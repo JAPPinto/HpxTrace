aee38acf4295ffb9606a0779e996b4834fe56c3c
z <jose982011@live.com.pt>
2021-02-21 17:49:30 +0000

    Added an example with simple counters implemented

---
+++ ex4-simple_counter.cpp
@@ -0,0 +1,67 @@
+/*
+Implementation of basic counters that monitor the number of heads and tails flipped.
+
+To test run with the following options:
+./ex4-simple_counter --hpx:print-counter=/test{locality#0/total}/heads --hpx:print-counter=/test{locality#0/total}/tails --hpx:print-counter-interval=1000
+
+
+*/
+
+#include <hpx/iostream.hpp>
+#include <hpx/include/performance_counters.hpp>
+#include <hpx/hpx_init.hpp>
+
+
+std::int64_t heads(0);
+std::int64_t tails(0);
+
+//Function that's called each time the counter "/test/heads" is read
+std::int64_t heads_counter(bool reset){
+    return heads;
+}
+//Function that's called each time the counter "/test/tails" is read
+
+std::int64_t tails_counter(bool reset){
+    return heads;
+}
+
+
+void register_counter_type() {
+    // Call the HPX API function to register the counter type.
+    hpx::performance_counters::install_counter_type(
+        "/test/heads",                                  // counter type name
+        &heads_counter,                                 // function providing counter data
+        "returns the number of heads"                   // description text (optional)
+        "heads"                                         // unit of measure (optional)
+    );
+    // Call the HPX API function to register the counter type.
+    hpx::performance_counters::install_counter_type(
+        "/test/tails",                                  // counter type name
+        &tails_counter,                                 // function providing counter data
+        "returns the number of taiks"                   // description text (optional)
+        "tails"                                         // unit of measure (optional)
+    );
+}
+
+
+int hpx_main()
+{
+    for (int i = 0; i < 300000000; ++i) {
+        if ((std::rand() % 2) == 0)
+            heads++;
+        else
+            tails++;
+    }
+
+    return hpx::finalize();
+}
+
+
+//HPX needs to be manually initialized so the counters can be registered first
+int main(int argc, char* argv[]) {
+
+    hpx::register_startup_function(&register_counter_type);
+
+    // Initialize and run HPX.
+    return hpx::init(argc, argv);
+}
